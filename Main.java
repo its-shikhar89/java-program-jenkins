0627d57b27290d1ebe29bfddd19fb5f0db5f446a
its-shikhar89 <shikhar895798@gmail.com>
2025-11-08 02:29:51 +0530

    Student Class Added

--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ public class Main {
     System.out.println(new Date());
     System.out.println("hello");
     System.out.println("Added one more file");
+
+    Student std = new Student();
+    std.sayHello();
   }
 }
